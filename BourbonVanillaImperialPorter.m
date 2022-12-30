f3b8c85501c35eb473a4c168b1a72d6369caf88f
Bill Kuker <bkuker@billkuker.com>
2022-12-30 12:54:23 -0500

    Create BourbonVanillaImperialPorter.m

---
+++ BourbonVanillaImperialPorter.m
@@ -0,0 +1,72 @@
+# Bourbon Vanilla Imperial Porter
+
+Target OG: 1.079
+Target FG: 1.017
+
+## Ingredients
+
+Denny's Favorite 50
+Vanilla bean
+Bourbon
+
+### Grain
+
+* 13#  2 Row Brewers Malt
+*  3#  Munich Malt
+* .5#  Biscuit Malt - Sub for brown malt
+*  1#  Chocolate Malt
+*  1#  Caramel / Crystal 40L
+* 1#  Caramel / Crystal 120L
+
+### Hops
+
+* Magnum Hops				1oz
+* E.K Goldings Hops		1oz
+
+## Method
+
+Smack yeast pack!
+
+### Mash
+
+Heat 6 Gallons strike water to 166°F
+
+(Strike temp calculator: Grain Temp ________________ Strike Temp ________________°F)
+
+Mash 19.5# grain @ 153°F for 75 minutes
+
+**Boil 2 Gallons water** for 10 minute mash-out @166°F
+
+Collect wort.
+
+Volume Collected: ______________________ G
+
+Pre-Boil Gravity: ______________________
+
+Calculated Target Volume: ______________________ G
+
+### Boil 60 minutes
+
+60 Minutes
+* 1 oz Magnum
+* .5 oz EKG
+
+10 Minutes
+* .5 oz EKG
+
+Gravity: ______________________
+
+Volume: ______________________ G
+
+### Pitch & Ferment
+
+Chill to 70°F
+
+Transfer to carboy
+
+Pitch
+
+### Extras
+
+Add vanilla bean after Primary (7-14 days)
+Add 1-1.75 cubs bourbon at bottling
